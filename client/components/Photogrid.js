4216da808e0110ed7452b35403da42a36a295900
Your Name <you@example.com>
2020-08-03 17:39:31 +0200

    first reducer

--- client/components/Photogrid.js
+++ client/components/Photogrid.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Photogrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                photogrid
+                {this.props.posts.map((post, index) => (
+                    <Photo 
+                        {...this.props} 
+                        key={index} 
+                        index={index} 
+                        post={post}
+                    />))
+                }
             </div>
         )
     }
--- client/reducers/postsReducer.js
+++ client/reducers/postsReducer.js
@@ -1,4 +1,16 @@
 export default function posts(state=[], action) {
-    console.log(state, action);
+    switch(action.type){
+        case 'INCREMENT_LIKES' :
+            console.log('incrementing likes');
+            const i = action.index;
+            return [
+                ...state.slice(0, i), // before the one that we are updating
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1), // after the one that we are updating
+            ]
+        default:
+            return state;
+
+    }
     return state;
 }
